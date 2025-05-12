dea394efcf43c4f5c15a883be2491b612d4adb8f
João Guilherme Souza Couto <168004754+JnGuilherme07@users.noreply.github.com>
2025-05-12 09:41:44 -0300

    9:41 12-5-25

--- SQL (exerciciosTRANSACTION AUTOMATIZADO) 8-05-25.sql
+++ SQL (exerciciosTRANSACTION AUTOMATIZADO) 8-05-25.sql
@@ -57,3 +57,90 @@ select * from Produto
 create database exTransationProcedure2
 use exTransationProcedure2
 
+CREATE TABLE Aluno (
+AlunoID INT PRIMARY KEY,
+NomeAluno VARCHAR(50)
+);
+
+CREATE TABLE SalasDeAula (
+SalaID INT PRIMARY KEY,
+NomeSala VARCHAR(50),
+Disponivel INT --0 indiponovel, 1 disponivel
+);
+
+INSERT INTO Aluno (AlunoID, NomeAluno) VALUES
+(1, 'João'),
+(2, 'Maria');
+
+INSERT INTO SalasDeAula (SalaID, NomeSala, Disponivel) VALUES
+(1, 'Sala A', 1),
+(2, 'Sala B', 1),
+(3, 'Sala C', 0);
+
+
+CREATE TABLE Disciplinas (
+DisciplinaID INT PRIMARY KEY,
+NomeDisciplina VARCHAR(50)
+);
+
+CREATE TABLE Matriculas (
+AlunoID INT,
+DisciplinaID INT,
+PRIMARY KEY (AlunoID, DisciplinaID)
+);
+
+INSERT INTO Disciplinas (DisciplinaID, NomeDisciplina) VALUES
+(1, 'Matemática'),
+(2, 'História');
+
+
+INSERT INTO Matriculas (AlunoID, DisciplinaID) VALUES
+(1, 1);
+
+
+CREATE PROCEDURE ReservarSalaDeAula
+	@SalaID INT,
+	@AlunoID INT,
+	@DisciplinaID INT
+AS
+BEGIN
+
+DECLARE @SalaDisponivel INT, @AlunoMatriculado INT;
+
+BEGIN TRANSACTION;
+	SELECT @SalaDisponivel = Disponivel
+	FROM SalasDeAula
+	WHERE SalaID = @SalaID;
+
+	SELECT @AlunoMatriculado = COUNT(*)
+	FROM Matriculas m
+	INNER JOIN Disciplinas d ON m.DisciplinaID = d.DisciplinaID
+	WHERE m.AlunoID = @AlunoID AND d.DisciplinaID = @DisciplinaID;
+
+	IF @SalaDisponivel = 1 AND @AlunoMatriculado = 1
+	BEGIN
+
+		UPDATE SalasDeAula SET Disponivel = 0 WHERE SalaID = @SalaID;
+
+		COMMIT;
+		PRINT 'Reserva concluída com sucesso.';
+	END
+
+	ELSE 
+	BEGIN
+		ROLLBACK;
+		PRINT 'Reserva não concluída. A sala não está disponível ou o aluno não está matriculado na disciplina correspondente.';
+	END
+END;
+
+
+
+EXEC ReservarSalaDeAula @SalaID = 2, @AlunoID = 1, @DisciplinaID = 1;
+
+update SalasDeAula set Disponivel=1 where SalaID=1
+
+SELECT * FROM Aluno
+SELECT * FROM SalasDeAula
+SELECT * FROM Disciplinas
+SELECT * FROM Matriculas
+
